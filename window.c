85a3f89c198ef5b912b7c66c99eda974a5d676de
gitantose <serra.1893530@studenti.uniroma1.it>
2022-09-29 20:26:49 +0200

    Window Layout 1

--- window.c
+++ window.c
@@ -5,39 +5,62 @@ WINDOW *my_win;
 int aux = 1;
 
 void* win1() {
+		initscr();
+		raw();
+		keypad(stdscr, TRUE);		/* I need that nifty F2 	*/
+		WINDOW* input = create_newwin(1, width, height, startx, 0);
+		mvprintw(height, startx, " > ");
+		move(height, startx+3);
+
     int ch;
 		while((ch = getch()) != KEY_F(2)) {
     }
 		aux = 0;
-}
-
-void* win2() {
-
-		while(aux) {
-
-			my_win = create_newwin(height, width, starty, startx);
-			sleep(1);
-					destroy_win(my_win);
-    }
+		destroy_win(input);
     endwin();
 }
 
-void window() {	
+void* win2() {
+		
+		while(aux) {
 
+			my_win = create_newwin(height, width, starty, startx, 1);
+			mvprintw(starty, (width-5)/2 , " My TOP ");
+			mvprintw(height - 1,(width-16)/2 , " Press F2 for exit ");
+			for (int i=startx+1; i<width-1; i++) {
+				for (int j=starty+1; j<height-3; j++) 
+					mvprintw(j, i, " ");
+			}
+			for (int i=startx + 1; i<(width-startx)/12; i++) {
+				for (int j=starty+5; j<height-3; j++)
+					mvprintw(starty + j, i*(width-startx)/12, "|");
+			}
+			for (int i=startx + 2; i<width-2; i++) {
+				mvprintw(starty + 5, i, "_");
+				mvprintw(starty + 9, i, "-");
+			}
+			move(height, startx+3);
+			refresh();
+			sleep(1);
+			
+    }
+    destroy_win(my_win);
+    endwin();
+}
 
-
+void window() {
     initscr();			/* Start curses mode 		*/
-
     raw();			/* Line buffering disabled, Pass on
 				 * everty thing to me 		*/
-    noecho();
+    //noecho();
 
     keypad(stdscr, TRUE);		/* I need that nifty F2 	*/
     getmaxyx(stdscr,height,width);		/* get the number of rows and columns */
+    height -= 1;
     starty = 0;	/* Calculating for a center placement */
     startx = 0;	/* of the window		*/
-    printw("Press F2 to exit");
-    refresh();
+    //printw("Press F2 to exit");
+    //refresh();
      
     //my_win = create_newwin(height, width, starty, startx);
     pthread_t thread[2];
@@ -49,19 +72,19 @@ void window() {
 		pthread_exit(NULL);    
 }
 
-WINDOW *create_newwin(int height, int width, int starty, int startx) {	
+WINDOW *create_newwin(int height, int width, int starty, int startx, int box) {	
     WINDOW *local_win;
     local_win = newwin(height, width, starty, startx);
-    box(local_win, 0 , 0);		/* 0, 0 gives default characters 
+    if (box) // solo se box è 1
+	    box(local_win, 0 , 0);		/* 0, 0 gives default characters 
 					   for the vertical and horizontal
 					    lines			*/
-		mvprintw(height - 2,(width-17)/2, "Press F2 for exit");
+
     wrefresh(local_win);		/* Show that box 		*/
     return local_win;
 }
 
-void destroy_win(WINDOW *local_win)
-{
+void destroy_win(WINDOW *local_win) {
     	/* box(local_win, ' ', ' '); : This won't produce the desired
   	 * result of erasing the window. It will leave it's four corners 
 	 * and so an ugly remnant of window.  */
--- window.h
+++ window.h
@@ -4,7 +4,7 @@
 #include "main.h"
 
 
-WINDOW *create_newwin(int height, int width, int starty, int startx);
+WINDOW *create_newwin(int height, int width, int starty, int startx, int box);
 void destroy_win(WINDOW *local_win);
 void window();
 	
